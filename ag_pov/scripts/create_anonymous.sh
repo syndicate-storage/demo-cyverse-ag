43a91cd14a4b0bfa4f0c96a73fb4875af11ad270
Illyoung Choi <iychoi@email.arizona.edu>
2016-12-14 21:51:09 -0700

    Reorganize directory

--- ag_pov/scripts/create_anonymous.sh
+++ ag_pov/scripts/create_anonymous.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 # create a user
-syndicate create_user "cyverse@cyverse.org" auto
 syndicate create_gateway email=anonymous volume=pov name=pov_reader private_key=auto type=UG caps=GATEWAY_CAP_READ_DATA|GATEWAY_CAP_READ_METADATA host=localhost port=31111
---
+++ ag_pov/scripts/create_user.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# create a user
+syndicate create_user "cyverse@cyverse.org" auto
