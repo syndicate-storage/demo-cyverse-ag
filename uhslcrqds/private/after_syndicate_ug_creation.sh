4b2a0c87839ababf7dd462f63e568ce1552c0738
Illyoung Choi <iychoi@email.arizona.edu>
2017-08-18 23:32:34 -0700

    Change owner/permission of ug_driver

--- uhslcrqds/private/after_syndicate_ug_creation.sh
+++ uhslcrqds/private/after_syndicate_ug_creation.sh
@@ -5,6 +5,9 @@ mkdir ${DRIVER_DIR}
 
 wget -O ${DRIVER_DIR}/driver https://raw.githubusercontent.com/syndicate-storage/syndicate-core/master/python/syndicate/ug/drivers/akamai/driver
 echo "{\"CDN_PREFIX\": \"http://hawaii-syn-cdn.opencloud.us\"}" > ${DRIVER_DIR}/config
+sudo chown -R syndicate:syndicate ${DRIVER_DIR}
+sudo chmod -R 744 ${DRIVER_DIR}
+
 
 echo "y" | syndicate update_gateway ${GATEWAY_NAME} driver=${DRIVER_DIR}
 if [ $? -ne 0 ]; then
